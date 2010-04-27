1e87b386a19d19a1d00cac019fc82558133ae7f3
Kazu Yamamoto <kazu@iij.ad.jp>
2010-04-27 10:29:29 +0900

    Using GHC API.

--- Lang.hs
+++ Lang.hs
@@ -1,14 +1,7 @@
 module Lang where
 
-import Control.Applicative
 import Param
-import System.IO
-import System.Process
+import DynFlags
 
 listLanguages :: Options -> IO String
-listLanguages opt = convert opt . lines <$> getLangs opt
-
-getLangs :: Options -> IO String
-getLangs opt = do
-  (_,hout,_,_) <- runInteractiveProcess (ghc opt) ["--supported-languages"] Nothing Nothing
-  hGetContents hout
+listLanguages opt = return $ convert opt supportedLanguages
