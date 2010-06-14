b548f1dbcfb9b517a918e2aeb886f9fcd4d24002
Kazu Yamamoto <kazu@iij.ad.jp>
2010-06-14 15:39:26 +0900

    pretty print.

--- elisp/ghc-flymake.el
+++ elisp/ghc-flymake.el
@@ -72,12 +72,11 @@
     (insert title "\n\n")
     (mapc (lambda (x) (insert (ghc-replace-character x ghc-null ghc-newline) "\n")) errs)
     (goto-char (point-min))
-    (while (re-search-forward "In the [^:\n]+: \\|Expected type: \\|Inferred type: " nil t)
+    (while (re-search-forward "In the [^:\n]+: \\|Expected type: \\|Inferred type: \\|Possible fix: " nil t)
       (replace-match (concat "\n" (match-string 0) "\n    ")))
     (goto-char (point-max))
     (while (re-search-backward "In the [a-z]+ argument" nil t)
       (insert "\n"))))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
