71ebb82eb248a0cbb0dc87c166839b06d32b1dcd
Kazu Yamamoto <kazu@iij.ad.jp>
2010-11-17 12:27:23 +0900

    Supporting GHC7 (Krzysztof Skrzętnicki).

--- Lang.hs
+++ Lang.hs
@@ -1,7 +1,13 @@
+{-# LANGUAGE CPP #-}
+
 module Lang where
 
 import DynFlags
 import Types
 
 listLanguages :: Options -> IO String
+#if __GLASGOW_HASKELL__ >= 700
+listLanguages opt = return $ convert opt supportedLanguagesAndExtensions
+#else
 listLanguages opt = return $ convert opt supportedLanguages
+#endif
