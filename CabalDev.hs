8bc72325eb88b3eaa94061d93a1b3b78e8a1d711
Clint Moore <cmoore@wamboli.com>
2011-02-16 01:46:28 -0800

    Ugh... yea, it is late.  This will fix it though, I think.

---
+++ CabalDev.hs
@@ -0,0 +1,36 @@
+
+module CabalDev (find_cabal_dev) where
+
+import System.Directory
+import System.Posix.Directory
+import System.FilePath.Posix
+
+find_cabal_dev :: IO [FilePath]
+find_cabal_dev = do
+    pth <- getWorkingDirectory
+    lx <- searchit $ splitPath pth
+    case lx of
+      Nothing -> return []
+      Just y -> return [y]
+ where
+   make_mod_path :: [FilePath] -> FilePath
+   make_mod_path x = do
+     let tp = joinPath x
+     case hasTrailingPathSeparator tp of
+       False -> tp ++ "/" ++ pkg_string
+       True -> tp ++ pkg_string
+
+   searchit :: [FilePath] -> IO (Maybe String)
+   searchit p =
+     case (length p) > 1 of
+       False -> return Nothing
+       True -> do
+           xx <- doesDirectoryExist $ (joinPath p) ++"/cabal-dev"
+           case xx of
+             False -> searchit $ init p
+             True -> return $ Just $ make_mod_path p
+   
+-- Can't seem to find anything on how to get the GHC version.
+-- It's late so I'll poke at it some more tomorrow.
+   pkg_string :: String
+   pkg_string = "cabal-dev/packages-6.12.3.conf:"
