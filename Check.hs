0defc980a807e32d84a81bdcc68b1cf970792c3b
Rodney Price <rod@thirdoption.info>
2011-03-27 16:43:11 -0600

    Commit my local changes.

--- Check.hs
+++ Check.hs
@@ -13,6 +13,8 @@ import Outputable hiding (showSDoc)
 import Prelude hiding (catch)
 import Pretty
 import System.FilePath
+import System.Directory
+import Text.Regex.Posix
 import Types
 
 ----------------------------------------------------------------
@@ -27,6 +29,8 @@ check opt fileName = withGHC $ do
     file <- initializeGHC opt fileName options
     setTargetFile file
     ref <- newRef []
+    initSession =<< liftIO parseGhciContents
+    setTargetFile fileName
     loadWithLogger (refLogger ref) LoadAllTargets `gcatch` handleParseError ref
     clearWarnings
     readRef ref
@@ -52,7 +56,10 @@ errBagToStrList = map showErrMsg . reverse . bagToList
 ----------------------------------------------------------------
 
 showErrMsg :: ErrMsg -> String
-showErrMsg err = file ++ ":" ++ line ++ ":" ++ col ++ ":" ++ msg ++ "\0" ++ ext
+showErrMsg err = 
+   if isGoodSrcSpan spn
+     then file ++ ":" ++ line ++ ":" ++ col ++ ":" ++ msg ++ "\0" ++ ext
+     else "bad span"
    where
      spn = head (errMsgSpans err)
      file = takeFileName $ unpackFS (srcSpanFile spn)
@@ -70,3 +77,40 @@ showSDoc d = map toNull . Pretty.showDocWith PageMode $ d style
   where
     toNull '\n' = '\0'
     toNull x = x
+
+----------------------------------------------------------------
+
+-- | Search this directory and all higher directories for .ghci files.
+-- The search ends when either (1) a .ghci file is found, (2) the
+-- user's home directory is encountered, or (3) the system root
+-- directory is encountered.  This function returns the contents of
+-- the .ghci file, or if no .ghci file is found, it returns a default
+-- string of settings.
+getGhciContents :: IO String
+getGhciContents = do
+  path <- getCurrentDirectory
+  exists <- doesFileExist (path ++ "/.ghci")
+  if exists
+    then readFile (path ++ "/.ghci")
+    else do
+      home <- getHomeDirectory
+      if path == home || path == "/"
+        then return defaultFlags
+        else do
+          setCurrentDirectory ".."
+          getGhciContents
+
+parseGhciContents :: IO [String]
+parseGhciContents = do
+  cwd <- getCurrentDirectory
+  ts <- getGhciContents
+  setCurrentDirectory cwd
+  let ls = filter (=~ "^:set ") (lines ts)
+  if length ls == 0
+    then return (findFlags [defaultFlags])
+    else return (findFlags ls)
+  where
+    findFlags = concat . map (filter (=~ "^-[fwW]") . words) 
+
+defaultFlags :: String
+defaultFlags = ":set -Wall -fno-warn-unused-do-bind"
