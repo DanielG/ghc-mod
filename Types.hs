eb93ebd8609a9d832274ab4d03bdb07c3733e3e3
Kazu Yamamoto <kazu@iij.ad.jp>
2013-03-01 09:43:50 +0900

    cosmetic change.

--- Types.hs
+++ Types.hs
@@ -9,7 +9,7 @@ data Options = Options {
   , hlintOpts    :: [String]
   , ghcOpts      :: [String]
   , operators    :: Bool
-  , detailed :: Bool
+  , detailed     :: Bool
   , expandSplice :: Bool
   , sandbox      :: Maybe String
   }
@@ -20,7 +20,7 @@ defaultOptions = Options {
   , hlintOpts    = []
   , ghcOpts      = []
   , operators    = False
-  , detailed = False
+  , detailed     = False
   , expandSplice = False
   , sandbox      = Nothing
   }
