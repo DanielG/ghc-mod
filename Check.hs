b35d19beec7a6752f0f7e2ee7b90b37b938be0b7
Kazu Yamamoto <kazu@iij.ad.jp>
2013-03-13 13:22:02 +0900

    adding a comment.

--- Check.hs
+++ Check.hs
@@ -23,6 +23,10 @@ check opt cradle fileName = withGHC fileName $ checkIt `gcatch` handleErrMsg
     checkIt = do
         readLog <- initializeFlagsWithCradle opt cradle options True
         setTargetFile fileName
+        -- To check TH, a session module graph is necessary.
+        -- "load" sets a session module graph using "depanal".
+        -- But we have to set "-fno-code" to DynFlags before "load".
+        -- So, this is necessary redundancy.
         slow <- needsTemplateHaskell <$> depanal [] False
         when slow setSlowDynFlags
         void $ load LoadAllTargets
