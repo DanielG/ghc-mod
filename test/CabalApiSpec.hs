24ada2abeb3047b1ec3d8b11814187debd1ad7ef
eagletmt <eagletmt@gmail.com>
2013-03-15 17:30:21 +0900

    Gather all hs-source-dirs to check test modules in sub-directories

--- test/CabalApiSpec.hs
+++ test/CabalApiSpec.hs
@@ -11,6 +11,11 @@ spec = do
             pkgs <- cabalAllDependPackages <$> cabalParseFile "test/data/cabalapi.cabal"
             pkgs `shouldBe` ["Cabal","base","template-haskell"]
 
+    describe "cabalAllSourceDirs" $ do
+        it "extracts all hs-source-dirs" $ do
+            dirs <- cabalAllSourceDirs <$> cabalParseFile "test/data/check-test-subdir/check-test-subdir.cabal"
+            dirs `shouldBe` ["src", "test"]
+
     describe "cabalBuildInfo" $ do
         it "extracts build info" $ do
             info <- cabalBuildInfo <$> cabalParseFile "test/data/cabalapi.cabal"
--- test/CheckSpec.hs
+++ test/CheckSpec.hs
@@ -3,6 +3,7 @@ module CheckSpec where
 import CabalApi
 import Check
 import Cradle
+import Data.List (isSuffixOf)
 import Expectation
 import Test.Hspec
 import Types
@@ -16,3 +17,9 @@ spec = do
                 cradle <- findCradle Nothing strVer
                 res <- checkSyntax defaultOptions cradle "main.hs"
                 res `shouldBe` "main.hs:5:1:Warning: Top-level binding with no type signature: main :: IO ()\NUL\n"
+
+        it "can check even if a test module imports another test module located at different directory" $ do
+            withDirectory_ "test/data/check-test-subdir" $ do
+                cradle <- getGHCVersion >>= findCradle Nothing . fst
+                res <- checkSyntax defaultOptions cradle "test/Bar/Baz.hs"
+                res `shouldSatisfy` ("test/Foo.hs:3:1:Warning: Top-level binding with no type signature: foo :: [Char]\NUL\n" `isSuffixOf`)
