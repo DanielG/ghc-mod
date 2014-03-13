9c30b11667507cddeff2c0a4eb416d672a29a2ef
Kazu Yamamoto <kazu@iij.ad.jp>
2014-03-13 09:34:45 +0900

    typo.

--- test/LintSpec.hs
+++ test/LintSpec.hs
@@ -6,7 +6,7 @@ import Test.Hspec
 spec :: Spec
 spec = do
     describe "lintSyntax" $ do
-        it "check syntax with HList" $ do
+        it "check syntax with HLint" $ do
             res <- lintSyntax defaultOptions "test/data/hlint.hs"
             res `shouldBe` "test/data/hlint.hs:4:8: Error: Redundant do\NULFound:\NUL  do putStrLn \"Hello, world!\"\NULWhy not:\NUL  putStrLn \"Hello, world!\"\n"
 
