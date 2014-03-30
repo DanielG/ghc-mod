5e01a452188400da4e96a57488b8d0ec261d84f0
Kazu Yamamoto <kazu@iij.ad.jp>
2014-03-30 15:07:14 +0900

    removing dep packages of benchmark.
    installing packages of benchmark fails in many cases, sigh.

--- Language/Haskell/GhcMod/CabalApi.hs
+++ Language/Haskell/GhcMod/CabalApi.hs
@@ -132,18 +132,13 @@ cabalCppOptions dir = do
 
 ----------------------------------------------------------------
 
--- | Extracting all 'BuildInfo' for libraries, executables, tests and benchmarks.
+-- | Extracting all 'BuildInfo' for libraries, executables, and tests.
 cabalAllBuildInfo :: PackageDescription -> [BuildInfo]
-cabalAllBuildInfo pd = libBI ++ execBI ++ testBI ++ benchBI
+cabalAllBuildInfo pd = libBI ++ execBI ++ testBI
   where
     libBI   = map P.libBuildInfo       $ maybeToList $ P.library pd
     execBI  = map P.buildInfo          $ P.executables pd
     testBI  = map P.testBuildInfo      $ P.testSuites pd
-#if __GLASGOW_HASKELL__ >= 704
-    benchBI = map P.benchmarkBuildInfo $ P.benchmarks pd
-#else
-    benchBI = []
-#endif
 
 ----------------------------------------------------------------
 
