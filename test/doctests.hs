17b80ccc2faecf30636b4f1e62f5c3ca8e461f5d
Kazu Yamamoto <kazu@iij.ad.jp>
2014-04-24 23:11:55 +0900

    doctest includes macro.

--- test/doctests.hs
+++ test/doctests.hs
@@ -6,5 +6,8 @@ main :: IO ()
 main = doctest [
     "-package"
   , "ghc"
+  , "-idist/build/autogen/"
+  , "-optP-include"
+  , "-optPdist/build/autogen/cabal_macros.h"
   , "Language/Haskell/GhcMod.hs"
   ]
