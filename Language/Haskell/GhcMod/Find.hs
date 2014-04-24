13c5c2c5af4f8cc24517e04fcbcff5f4cef54439
Kazu Yamamoto <kazu@iij.ad.jp>
2014-04-24 22:11:06 +0900

    doc.

--- Language/Haskell/GhcMod/Find.hs
+++ Language/Haskell/GhcMod/Find.hs
@@ -31,6 +31,7 @@ type Symbol = String
 -- | Database from 'Symbol' to modules.
 newtype SymMdlDb = SymMdlDb (Map Symbol [ModuleString])
 
+-- | Find modules to which the symbol belong.
 findSymbol :: Options -> Cradle -> Symbol -> IO String
 findSymbol opt cradle sym = withGHC' $ do
     void $ initializeFlagsWithCradle opt cradle [] False
