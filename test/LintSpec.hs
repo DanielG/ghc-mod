151673b58cdb36efbed3873fa360263e6d2d71e9
Kohei Suzuki <eagletmt@gmail.com>
2014-05-11 00:25:44 +0900

    Add test for no errors case

--- test/LintSpec.hs
+++ test/LintSpec.hs
@@ -10,3 +10,7 @@ spec = do
             res <- lintSyntax defaultOptions "test/data/hlint.hs"
             res `shouldBe` "test/data/hlint.hs:4:8: Error: Redundant do\NULFound:\NUL  do putStrLn \"Hello, world!\"\NULWhy not:\NUL  putStrLn \"Hello, world!\"\n"
 
+        context "without suggestions" $ do
+            it "doesn't output empty line" $ do
+                res <- lintSyntax defaultOptions "test/data/ghc-mod-check/Data/Foo.hs"
+                res `shouldBe` ""
