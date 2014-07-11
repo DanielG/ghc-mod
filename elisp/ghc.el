1f149123fa23420a467fc122b6287c6654ff1aef
Kazu Yamamoto <kazu@iij.ad.jp>
2014-07-11 19:05:54 +0900

    ver bumps up

--- elisp/ghc.el
+++ elisp/ghc.el
@@ -28,7 +28,7 @@
 	       (< emacs-minor-version minor)))
       (error "ghc-mod requires at least Emacs %d.%d" major minor)))
 
-(defconst ghc-version "4.1.4")
+(defconst ghc-version "4.1.5")
 
 ;; (eval-when-compile
 ;;  (require 'haskell-mode))
