de4fc7642d05750458723dfb3a26838db45a4fa6
Daniel Gröber <dxld@darkboxed.org>
2014-10-04 00:02:21 +0200

    Bump version to v5.1.1.0

--- elisp/ghc.el
+++ elisp/ghc.el
@@ -28,7 +28,7 @@
 	       (< emacs-minor-version minor)))
       (error "ghc-mod requires at least Emacs %d.%d" major minor)))
 
-(defconst ghc-version "5.1.0.2")
+(defconst ghc-version "v5.1.1.0")
 
 ;; (eval-when-compile
 ;;  (require 'haskell-mode))
