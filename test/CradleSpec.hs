84888627af04ccd45e34ff96d13363868c9795b1
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2015-04-26 17:33:01 +0200

    Fixed custom cradle test

--- test/CradleSpec.hs
+++ test/CradleSpec.hs
@@ -71,6 +71,8 @@ spec = do
                 cradleCabalFile  res `shouldBe`
                   Just ("test" </> "data" </> "broken-sandbox" </> "dummy.cabal")
 
+                cradlePkgDbStack res `shouldBe` [GlobalDb, UserDb]
+
         it "uses the custom cradle file if present" $ do
             withDirectory "test/data/custom-cradle" $ \dir -> do
                 res <- relativeCradle dir <$> findCradle
@@ -78,4 +80,3 @@ spec = do
                 cradleRootDir res    `shouldBe` "test" </> "data" </> "custom-cradle"
                 cradleCabalFile res  `shouldBe` Just ("test" </> "data" </> "custom-cradle" </> "dummy.cabal")
                 cradlePkgDbStack res `shouldBe` [PackageDb "a/packages", GlobalDb, PackageDb "b/packages", UserDb, PackageDb "c/packages"]
-                cradlePkgDbStack res `shouldBe` [GlobalDb, UserDb]
