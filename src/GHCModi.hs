2cd4d6bd8035022db4d35c61e0258f515f4bf39c
Daniel Gröber <dxld@darkboxed.org>
2015-08-10 11:10:00 +0200

    Bind ghc-modi executable to right ghc-mod exe

--- src/GHCModi.hs
+++ src/GHCModi.hs
@@ -5,16 +5,51 @@
 
 module Main where
 
+import Control.Applicative
+import Control.Monad
+import Control.Exception
+import Data.Version
+import Data.Maybe
+import System.IO
 import System.Exit
 import System.Process
 import System.FilePath
 import System.Environment
 import Paths_ghc_mod
+import Utils
+import Prelude
 
 main :: IO ()
 main = do
+  hPutStrLn stderr $
+    "Warning: ghc-modi is deprecated please use 'ghc-mod legacy-interactive' instead"
+
   args <- getArgs
   bindir <- getBinDir
-  (_, _, _, h) <-
-    createProcess $ proc (bindir </> "ghc-mod") $ ["legacy-interactive"] ++ args
-  exitWith =<< waitForProcess h
+  let installedExe = bindir </> "ghc-mod"
+  mexe <- mplus <$> mightExist installedExe <*> pathExe
+  case mexe of
+    Nothing -> do
+      hPutStrLn stderr $
+        "ghc-modi: Could not find '"++installedExe++"', check your installation!"
+      exitWith $ ExitFailure 1
+
+    Just exe -> do
+      (_, _, _, h) <-
+          createProcess $ proc exe $ ["legacy-interactive"] ++ args
+      exitWith =<< waitForProcess h
+
+pathExe :: IO (Maybe String)
+pathExe = do
+  ev <- try $ words <$> readProcess "ghc-mod" ["--version"] ""
+  let mexe = case ev of
+               Left (SomeException _) -> Nothing
+               Right ["ghc-mod", "version", ver
+                     , "compiled", "by", "GHC", _]
+                   | showVersion version == ver -> do
+                       Just "ghc-mod"
+               Right _ -> Nothing
+
+  when (isNothing mexe) $
+      hPutStrLn stderr "ghc-modi: ghc-mod executable on PATH has different version, check your installation!"
+  return mexe
