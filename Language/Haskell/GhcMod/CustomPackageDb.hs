6f0b8b00d1c95f9a62235c257bcb5a2d4d34d3a5
Daniel Gröber <dxld@darkboxed.org>
2015-11-26 15:03:14 +0100

    Add some missing copyright boilerplate

--- Language/Haskell/GhcMod/CustomPackageDb.hs
+++ Language/Haskell/GhcMod/CustomPackageDb.hs
@@ -1,5 +1,19 @@
+-- ghc-mod: Making Haskell development *more* fun
+-- Copyright (C) 2015  Daniel Gröber <dxld ÄT darkboxed DOT org>
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 module Language.Haskell.GhcMod.CustomPackageDb where
-
 
 import Control.Applicative
 import Control.Monad
@@ -10,7 +24,6 @@ import Language.Haskell.GhcMod.Types
 import Language.Haskell.GhcMod.Monad.Types
 import Language.Haskell.GhcMod.PathsAndFiles
 import Prelude hiding ((.))
-
 
 parseCustomPackageDb :: String -> [GhcPkgDb]
 parseCustomPackageDb src = map parsePkgDb $ filter (not . null) $ lines src
