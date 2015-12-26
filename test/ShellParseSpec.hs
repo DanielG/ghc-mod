fff9087ff75a7abe1fd33280f449fe4d9f154b33
Nikolay Yakimov <root@livid.pp.ru>
2015-12-27 02:42:45 +0300

    [Shell-Parse] Use \STX and \ETX for quoting
    Also drops escaping

--- test/ShellParseSpec.hs
+++ test/ShellParseSpec.hs
@@ -10,27 +10,9 @@ spec =
   describe "parseCmdLine" $ do
     it "splits arguments" $
       parseCmdLine "test command line" `shouldBe` ["test", "command", "line"]
-    it "honors double quotes" $
-      parseCmdLine "test command line \"with double quotes\""
-        `shouldBe` ["test", "command", "line", "with double quotes"]
-    it "escapes spaces" $ do
-      parseCmdLine "with\\ spaces"
-        `shouldBe` ["with spaces"]
-      parseCmdLine "\"with\\ spaces\""
-        `shouldBe` ["with spaces"]
-    it "escapes '\\'" $ do
-      parseCmdLine "\\\\"
-        `shouldBe` ["\\"]
-      parseCmdLine "\"\\\\\""
-        `shouldBe` ["\\"]
-    it "escapes double quotes" $ do
-      parseCmdLine "\\\""
-        `shouldBe` ["\""]
-      parseCmdLine "\"\\\"\""
-        `shouldBe` ["\""]
-    it "doesn't escape random characters" $
-      parseCmdLine "\\a\\b\\c"
-        `shouldBe` ["\\a\\b\\c"]
+    it "honors quoted segments" $
+      parseCmdLine "test command line \STXwith quoted segment\ETX"
+        `shouldBe` ["test", "command", "line", "with quoted segment"]
     it "squashes multiple spaces" $
       parseCmdLine "test       command"
         `shouldBe` ["test", "command"]
