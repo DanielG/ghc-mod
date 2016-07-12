24852213e1c80ae790136fb961b22fd008f5dcf6
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2016-07-13 08:39:47 +1000

    Merging the ghc-imported-from utility.

---
+++ test/data/imported-from/ImportedFrom03.hs
@@ -0,0 +1,15 @@
+-- ImportedFrom03.hs
+
+module ImportedFrom03 where
+
+import Control.Monad ( forM_, liftM, filterM, when, unless )
+import Control.Monad.Identity
+import Control.Monad.Reader
+import Control.Monad.Trans.Writer.Lazy
+
+
+
+
+
+main = do
+    when True $ do print "hah"
