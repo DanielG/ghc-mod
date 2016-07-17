b94f2f4e635a2785ce0fdc8bb6ce23e72543f5dc
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2016-07-17 21:32:13 +0800

    Adjust ghc version bound on ghcIdeclHiding.

--- Language/Haskell/GhcMod/Gap.hs
+++ Language/Haskell/GhcMod/Gap.hs
@@ -700,12 +700,14 @@ ghcQualify :: PrintUnqualified
 ghcQualify = alwaysQualify
 #endif
 
-#if __GLASGOW_HASKELL__ >= 708
+#if __GLASGOW_HASKELL__ >= 710
 ghcIdeclHiding :: GHC.ImportDecl GHC.RdrName -> Maybe (Bool, SrcLoc.Located [GHC.LIE GHC.RdrName])
 ghcIdeclHiding = GHC.ideclHiding
 #else
--- In ghc-7.6.3, we have
+-- Here, we have
+--
 --     ideclHiding :: Maybe (Bool, [LIE name])
+--
 -- so we have to use noLoc to get a SrcLoc.Located type in the second part of the tuple.
 ghcIdeclHiding :: GHC.ImportDecl GHC.RdrName -> Maybe (Bool, SrcLoc.Located [GHC.LIE GHC.RdrName])
 ghcIdeclHiding x = case GHC.ideclHiding x of
