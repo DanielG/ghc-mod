0a56669151323a5ff1bce9666a2aff28301788e6
Nikolay Yakimov <root@livid.pp.ru>
2016-10-23 14:27:22 +0300

    Imported-from spec update

--- test/ImportedFromSpec.hs
+++ test/ImportedFromSpec.hs
@@ -1,23 +1,14 @@
 {-# LANGUAGE CPP #-}
 module ImportedFromSpec where
 
-import Control.Applicative
 import Data.List
 import Language.Haskell.GhcMod
-import System.FilePath
 import Test.Hspec
 import TestUtils
 import Prelude
 
-import Language.Haskell.GhcMod.Utils
-
 ---------------------------------------------------
 import Language.Haskell.GhcMod.ImportedFrom
-import System.FilePath()
-import Test.Hspec
-
-import Control.Exception as E
-import System.Directory
 ---------------------------------------------------
 
 spec :: Spec
@@ -31,42 +22,42 @@ spec = do
         -- was giving the contents of the body of the function. This worked
         -- before???
         it "can look up Maybe" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 22 17 (Expression "Maybe")
+            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 22 17 (Just (Expression "Maybe"))
             res `shouldSatisfy` (\x -> "base-" `isInfixOf` x || "haskell98-" `isInfixOf` x || "haskell2010-" `isInfixOf` x)
             res `shouldSatisfy` (isInfixOf "Data-Maybe.html")
 
         it "can look up Just" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 12 7  (Expression "Just")
+            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 12 7  (Just (Expression "Just"))
             res `shouldSatisfy` (\x -> "base-" `isInfixOf` x || "haskell98-" `isInfixOf` x || "haskell2010-" `isInfixOf` x)
             res `shouldSatisfy` (isInfixOf "Data-Maybe.html")
 
         it "can look up Just" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 16 10 (Expression "Just")
+            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 16 10 (Just (Expression "Just"))
             res `shouldSatisfy` (\x -> "base-" `isInfixOf` x || "haskell98-" `isInfixOf` x || "haskell2010-" `isInfixOf` x)
             res `shouldSatisfy` (isInfixOf "Data-Maybe.html")
 
         it "can look up String" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 20 14 (Expression "String")
+            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 20 14 (Just (Expression "String"))
             res `shouldSatisfy` (\x -> "base-" `isInfixOf` x || "haskell98-" `isInfixOf` x || "haskell2010-" `isInfixOf` x)
             res `shouldSatisfy` (isInfixOf "Prelude.html")
 
         it "can look up Int" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 22 23 (Expression "Int")
+            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 22 23 (Just (Expression "Int"))
             res `shouldSatisfy` (\x -> "base-" `isInfixOf` x || "haskell98-" `isInfixOf` x || "haskell2010-" `isInfixOf` x)
             res `shouldSatisfy` (isInfixOf "Prelude.html")
 
         it "can look up DL.length" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 23 5  (Expression "DL.length")
+            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 23 5  (Just (Expression "DL.length"))
             res `shouldSatisfy` (\x -> "base-" `isInfixOf` x || "haskell98-" `isInfixOf` x || "haskell2010-" `isInfixOf` x)
             res `shouldSatisfy` (isInfixOf "Data-List.html")
 
         it "can look up print" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 25 8  (Expression "print")
+            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 25 8  (Just (Expression "print"))
             res `shouldSatisfy` (\x -> "base-" `isInfixOf` x || "haskell98-" `isInfixOf` x || "haskell2010-" `isInfixOf` x)
             res `shouldSatisfy` (isInfixOf "Prelude.html")
 
         it "can look up DM.fromList" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 27 5  (Expression "DM.fromList")
+            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 27 5  (Just (Expression "DM.fromList"))
             res `shouldSatisfy` (isInfixOf "containers-")
             res `shouldSatisfy` (isInfixOf "Data-Map.html")
 
@@ -76,26 +67,26 @@ spec = do
         --
         --it "can look up Safe.headMay" $ do
         --    withDirectory_ "test/data/imported-from" $ do
-        --        (res, _) <- runGmOutDef $ runGhcModT defaultOptions $ importedFrom "ImportedFrom01.hs" 29 6  (Expression "Safe.headMay")
+        --        (res, _) <- runGmOutDef $ runGhcModT defaultOptions $ importedFrom "ImportedFrom01.hs" 29 6  (Just (Expression "Safe.headMay"))
         --        res `shouldSatisfy` isRight
 
         it "can look up Foo.Bar.length" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 34 17 (Expression "Foo.Bar.length")
+            res <- runD' tdir $ importedFrom "ImportedFrom01.hs" 34 17 (Just (Expression "Foo.Bar.length"))
             res `shouldSatisfy` (\x -> "base-" `isInfixOf` x || "haskell98-" `isInfixOf` x || "haskell2010-" `isInfixOf` x)
             res `shouldSatisfy` (isInfixOf "Data-List.html")
 
         -- These from Safe also fail. Why?
         --it "can look up map" $ do
-        --    res <- runD' tdir $ importedFrom "ImportedFrom02.hs" 14 5  (Expression "map")
+        --    res <- runD' tdir $ importedFrom "ImportedFrom02.hs" 14 5  (Just (Expression "map"))
         --    res `shouldSatisfy` (isInfixOf "000")
         --    res `shouldSatisfy` (isInfixOf "111")
 
         --it "can look up head" $ do
-        --    res <- runD' tdir $ importedFrom "ImportedFrom02.hs" 16 5  (Expression "head")
+        --    res <- runD' tdir $ importedFrom "ImportedFrom02.hs" 16 5  (Just (Expression "head"))
         --    res `shouldSatisfy` (isInfixOf "000")
         --    res `shouldSatisfy` (isInfixOf "111")
 
         it "can look up when" $ do
-            res <- runD' tdir $ importedFrom "ImportedFrom03.hs"   15 5  (Expression "when")
+            res <- runD' tdir $ importedFrom "ImportedFrom03.hs"   15 5  (Just (Expression "when"))
             res `shouldSatisfy` (\x -> "base-" `isInfixOf` x || "haskell98-" `isInfixOf` x || "haskell2010-" `isInfixOf` x)
             res `shouldSatisfy` (isInfixOf "Control-Monad.html")
