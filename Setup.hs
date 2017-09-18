9b3f3468079765c593ad6ed7d7a9c701a33fdadb
Daniel Gröber <dxld@darkboxed.org>
2017-09-18 12:27:45 +0200

    Fix doctest with Cabal-2.0 by requiring c-i>=1.24

--- Setup.hs
+++ Setup.hs
@@ -1,9 +1,23 @@
 #!/usr/bin/env runhaskell
 
+{-# LANGUAGE CPP #-}
+
+#ifndef MIN_VERSION_cabal_doctest
+#error \
+    Your version of cabal-install does not seem to support the 'custom-setup' section. \
+    Please see https://github.com/DanielG/ghc-mod/wiki/Installing#checking-and-installing-prerequisites for instrutions on how to upgrade. \
+    It is also possible that you forgot to install cabal-doctest before running Setup.hs
+#endif
+
 import Distribution.Simple
 import Distribution.Simple.Program
 
+import Distribution.Extra.Doctest
+
 main :: IO ()
-main = defaultMainWithHooks $ simpleUserHooks {
-   hookedPrograms = [ simpleProgram "shelltest" ]
- }
+main =
+    defaultMainWithHooks $
+    addDoctestsUserHook "doctest" $
+    simpleUserHooks {
+        hookedPrograms = [ simpleProgram "shelltest" ]
+    }
