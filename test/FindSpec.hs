b72db576c0ac56e15cbf5fc56e09523f8275d4c9
Daniel Gröber <dxld@darkboxed.org>
2018-08-23 23:08:34 +0200

    Fix loadSymbolDb test when using new-build

--- test/FindSpec.hs
+++ test/FindSpec.hs
@@ -4,10 +4,12 @@ module FindSpec where
 import GhcMod.Exe.Find
 import Test.Hspec
 import TestUtils
+import Dir
 
 spec :: Spec
 spec = do
     describe "db <- loadSymbolDb" $ do
         it "lookupSymbol' db \"head\"  contains at least `Data.List'" $ do
-            db <- runD $ loadSymbolDb
-            lookupSym "head" db `shouldContain` [ModuleString "Data.List"]
+            withDirectory_ "test/data/ghc-mod-check" $ do
+                db <- runD $ loadSymbolDb
+                lookupSym "head" db `shouldContain` [ModuleString "Data.List"]
