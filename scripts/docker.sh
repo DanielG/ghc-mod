14746ddc8a7abd3dda089c88347594ac3189b91c
Daniel Gröber <dxld@darkboxed.org>
2018-12-07 17:50:30 +0100

    scripts: Upgrade all docker images to Cabal-2.4.0.0

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,7 +3,7 @@
 # Usage: ./docker.sh [GHC_VER]
 # Example: ./docker.sh 8.2.2
 # Faster Example:
-#     $ echo 8.6.1 8.4.4 8.2.2 8.0.2 7.10.3 7.8.4 | xargs -P4 -n1 -- ./docker.sh
+#     $ echo 8.6.2 8.4.4 8.2.2 8.0.2 7.10.3 | xargs -P4 -n1 -- ./docker.sh
 
 
 namespace="registry.gitlab.com/dxld/ghc-mod"
@@ -19,7 +19,6 @@ while read -r ghc_rel ghc ghc_arch ghc_ext cabal cabal_rev image; do
 
     ghc_url="https://downloads.haskell.org/~ghc/${ghc_rel}/ghc-${ghc}-${ghc_arch}.tar.${ghc_ext}"
     cabal_url="https://hackage.haskell.org/package/cabal-install-${cabal}/cabal-install-${cabal}.tar.gz"
-    # https://www.haskell.org/cabal/release/cabal-install-${cabal}/cabal-install-${cabal}.tar.gz
     cabal_meta_url="https://hackage.haskell.org/package/cabal-install-${cabal}/revision/${cabal_rev}.cabal"
 
     ghc_file="$(basename "$ghc_url")"
@@ -27,8 +26,21 @@ while read -r ghc_rel ghc ghc_arch ghc_ext cabal cabal_rev image; do
     cabal_meta_file=cabal-install-"${cabal}"-"${cabal_rev}".cabal
 
     ADDITIONAL_PACKAGES=""
+    ADDITIONAL_PURGE_PACKAGES=""
     ADDITIONAL_COMMANDS=""
     ADDITIONAL_BOOTSTRAP_SETUP="true"
+    ADDITIONAL_BOOTSTRAP_TEARDOWN="true"
+
+    if [ $ghc_rel = "7.10.3" ]; then
+            cabal_boot=2.2.0.0
+            cabal_bin_url="https://www.haskell.org/cabal/release/cabal-install-${cabal_boot}/cabal-install-${cabal_boot}-x86_64-unknown-linux.tar.gz"
+            cabal_bin_file="$(basename "$cabal_bin_url")"
+            wget -nv -nc -c "$cabal_bin_url"  && cp -l "$cabal_bin_file" "$tmpdir"
+            ADDITIONAL_COMMANDS="${ADDITIONAL_COMMANDS}COPY $cabal_bin_file /root/
+"
+            ADDITIONAL_BOOTSTRAP_SETUP='tar -C /root -xf /root/'"$cabal_bin_file"' cabal && /root/cabal update && /root/cabal install --only-dependencies'
+            ADDITIONAL_BOOTSTRAP_TEARDOWN='rm /root/cabal '"$cabal_bin_file"
+    fi
 
     mkdir -p "$dldir"
     d="$(pwd)"; cd "$dldir" || exit 1
@@ -41,7 +53,7 @@ while read -r ghc_rel ghc ghc_arch ghc_ext cabal cabal_rev image; do
     ls -l "$tmpdir"
     cd "$d" || exit 1
 
-    ADDITIONAL_COMMANDS="${ADDITIONAL_COMMANDS}COPY $ghc_file $cabal_file $cabal_meta_file /root/
+    ADDITIONAL_COMMANDS="${ADDITIONAL_COMMANDS}COPY $ghc_file $cabal_file $cabal_bin_file $cabal_meta_file /root/
 "
 
     cat > "$tmpdir"/Dockerfile <<EOF
@@ -61,24 +73,29 @@ RUN apt-get update && apt-get upgrade -y && \
 RUN tar -xf ghc-*.tar.* && \
     cd ghc-* && ./configure --prefix=/usr/local && make install && cd .. && \
     rm -r ghc-*
-RUN tar -xf cabal-install-*.tar.* && \
-    cd cabal-install-* && \
+RUN tar -xf cabal-install-${cabal}.tar.* && \
+    cd cabal-install-*/ && \
       cp /root/$cabal_meta_file cabal-install.cabal && \
       ${ADDITIONAL_BOOTSTRAP_SETUP} && \
-      ./bootstrap.sh --no-doc --global && \
+      sh -x ./bootstrap.sh --no-doc --user && \
+      cp ~/.cabal/bin/cabal /usr/local/bin && \
+      rm -r ~/.cabal ~/.ghc && \
+      ${ADDITIONAL_BOOTSTRAP_TEARDOWN} && \
     cd .. && \
+    ls -l /root && \
     rm -r cabal-*
 EOF
 
     docker build -t "${namespace}:ghc${ghc}-cabal-install${cabal}" "$tmpdir"
 done <<EOF
-8.6.1  8.6.1  x86_64-deb8-linux         xz  2.4.0.0   0 debian:jessie
-8.4.4  8.4.4  x86_64-deb8-linux         xz  2.2.0.0   0 debian:jessie
-8.2.2  8.2.2  x86_64-deb8-linux         xz  2.0.0.0   0 debian:jessie
-8.0.2  8.0.2  x86_64-deb8-linux         xz  2.0.0.0   0 debian:jessie
-7.10.3 7.10.3 x86_64-deb8-linux         xz  2.0.0.0   0 debian:jessie
-7.8.4  7.8.4  x86_64-unknown-linux-deb7 xz  2.0.0.0   0 debian:jessie
+8.6.2  8.6.2  x86_64-deb8-linux         xz  2.4.0.0   0 debian:jessie
+8.4.4  8.4.4  x86_64-deb8-linux         xz  2.4.0.0   0 debian:jessie
+8.2.2  8.2.2  x86_64-deb8-linux         xz  2.4.0.0   0 debian:jessie
+8.0.2  8.0.2  x86_64-deb8-linux         xz  2.4.0.0   0 debian:jessie
+7.10.3 7.10.3 x86_64-deb8-linux         xz  2.4.0.0   0 debian:jessie
 EOF
+
+# 7.8.4  7.8.4  x86_64-unknown-linux-deb7 xz  2.2.0.0   0 debian:jessie
 
 # 8.0.2  8.0.2  x86_64-deb8-linux         xz  1.24.0.2  0 debian:jessie
 # 7.10.3 7.10.3 x86_64-deb8-linux         xz  1.24.0.2  0 debian:jessie
